709d6b50da9f0850ba30869ea25e87a986a195a7
Travis Clark <travis.clark490@myci.csuci.edu>
2025-04-25 20:59:21 -0700

    added POST task endpoint, user can add json task

--- run.py
+++ run.py
@@ -1,16 +1,37 @@
-from flask import Flask, jsonify #import jsonify, flask function that converts python data to JSON
+from flask import Flask, jsonify, request #jsonify, flask function that converts python data to JSON
 
 app = Flask(__name__)
 
 tasks = [] #instantiate task dictionary
+next_id = 1 # task counter
 
-@app.route('/') #home endpoint
+# Root endpoint
+@app.route('/')
 def home():
     return "Task Manager API is running..."
 
-@app.route('/tasks', methods=['GET']) #tasks endpoint, returns tasks in JSON
+# GET /tasks: Returns tasks as JSON
+@app.route('/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify(tasks), 200 
+    return jsonify(tasks), 200 #successful request
+
+# POST /tasks: Creates new tasks from JSON formatted input
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global next_id
+    data = request.get_json() # get json from request
+
+    # Validate 'title' is given
+    if not data or 'title' not in data:
+        return jsonify({"error" : "Task title is required"}), 400 #bad request
+
+    # Create task and add to list
+    task = {"id": next_id, "title": data["title"]}
+    tasks.append(task)
+    next_id += 1 #Increment id for next task
+
+    return jsonify(task), 201 #resource created
+
 
 if __name__ == '__main__':
     app.run(debug=True)
