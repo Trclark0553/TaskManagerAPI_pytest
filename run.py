a6207d9678fd9aefe94204fce4b316b430d22d56
Travis Clark <travis.clark490@myci.csuci.edu>
2025-04-25 18:57:45 -0700

    add tasks endpoint, no tasks yet

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify #import jsonify, flask function that converts python data to JSON
 
 app = Flask(__name__)
 
-@app.route('/')
+tasks = [] #instantiate task dictionary
+
+@app.route('/') #home endpoint
 def home():
     return "Task Manager API is running..."
+
+@app.route('/tasks', methods=['GET']) #tasks endpoint, returns tasks in JSON
+def get_tasks():
+    return jsonify(tasks), 200 
 
 if __name__ == '__main__':
     app.run(debug=True)
