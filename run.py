b544932b6c2dbf88bfef4e301d8ab2b76fb22e90
Travis Clark <travis.clark490@myci.csuci.edu>
2025-04-26 11:10:05 -0700

    add logic to reorder task list

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request #jsonify, flask function that converts
 app = Flask(__name__)
 
 tasks = [] #instantiate task dictionary
-next_id = 1 # task counter
+next_id = 1 #task counter
 
 # Root endpoint
 @app.route('/')
@@ -35,19 +35,29 @@ def create_task():
 # DELETE /tasks/task_id. Ex: curl -x delete http://localhost:5000/tasks/1
 @app.route('/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    global tasks
+    global tasks, next_id
 
     #loop through list to find first match, if not found set default=None
-    task = next((t fo t in tasks if t["id"] == task_id), None)
+    task = next((t for t in tasks if t["id"] == task_id), None)
 
     #report to user task not found, return 404
     if not task:
-        return jsonify({"error": "Task not found"}), 404
+        return jsonify({"error": f"Task {task_id} not found."}), 404
 
-    #modify list to exclude target task id
+    #remove task from list
     tasks = [t for t in tasks if t["id"] != task_id]
 
-    return '', 204
+    #reorder remaining list items. 2->1 if 1 is deleted
+    for idx, task in enumerate(tasks, start=1):
+        task["id"] = idx
+
+    next_id = len(tasks) + 1
+
+    if not tasks:
+        return jsonify({"message": f"Task {task_id} deleted. List is empty!"}), 200
+    else:
+        return jsonify({"message": f"Task {task_id} deleted. {len(tasks)} task(s) remaining."}), 200
+    
 
 
 if __name__ == '__main__':
